522455c4e48af77df35378d4053601478457b7e6
patriziapatti <patrizia.patti5@gmail.com>
2024-09-10 19:15:15 +0100

    primo commit

---
+++ backend/routes/authenticationRoutes.js
@@ -0,0 +1,18 @@
+import express from 'express';
+// import {register,login,me, callbackGoogle} from '../controllers/authentication.controller.js';
+// import authorization from '../middleware/authorization.js';
+import passport from 'passport';
+// import uploadCloudinary from '../middleware/uploadCloudinary.js'
+
+const authenticationRouter = express.Router()
+
+authenticationRouter.post('/register',uploadCloudinary.single('avatar'), register);
+authenticationRouter.post('/login', login);
+authenticationRouter.get('/me',authorization, me);
+
+
+authenticationRouter.get('/login-google', passport.authenticate('google', {scope:['profile','email']}))// qui chiamo un middlware di passport che ci ridireziona alla pagina di google
+authenticationRouter.get('/callback-google',passport.authenticate('google', {session: false}), callbackGoogle)//per disattivare i cookies
+
+
+export default authenticationRouter;
---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import 'dotenv/config';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors'
+import morgan from 'morgan';
+import helmet from 'helmet'
+// import authenticationRouter from './routes/authenticationRoutes.js';
+import passport from 'passport';
+// import googleStrategy from './config/passport.config.js';
+// import authorization from './middleware/authorization.js';
+
+const port = process.env.PORT || 5000;
+
+//creo il server
+const server = express()
+
+//passport.use('google', googleStrategy)//non è un middleware ma serve per dire a passport di usare la strategia
+
+//collegamento al db
+await mongoose.connect(process.env.MONGO_CONNECTION_URI).then(()=>{
+    console.log('connessione al db ok')
+}).catch((err)=> {console.log(err)})
+
+server.use(express.json())// è un middleware che ci dice tutti i body che invieremo saranno in json
+server.use(cors()) // è un middleware che consente la connessione tra backend e frontend
+server.use(morgan("dev"))// è un middleware che mi mostra tutti i log delle richieste
+server.use(helmet ())//middleware che ci da la sicurezza per il backend
+
+// server.use("/auth",authenticationRouter)
+
+
+server.listen(port, ()=>{
+    console.log('Server is running')
+})
