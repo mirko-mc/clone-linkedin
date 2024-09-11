d9f1b7beda5b46e8b3c09e8a93121ea38e3246f0
mirko-mc <8547740+mirko-mc@users.noreply.github.com>
2024-09-11 13:34:36 +0200

    BACKEND - fixed on variable - renaming experiences router - base post experience request in request.http - created experience schema - created post experience in experience controller

--- backend/routes/experiences.routes.js
+++ backend/routes/experiences.routes.js
@@ -1,17 +1,26 @@
 import express from "express";
 import * as ExperiencesController from "../controllers/experiences.controller.js";
-const Router = express.Router();
+const ExperiencesRouter = express.Router();
 
 // TODO GET /:userld/experiences => ritorna TUTTE le esperienze di un utente
-Router.get("/:userld/experiences", ExperiencesController.GetAllExperiences);
+ExperiencesRouter.get(
+  "/:userld/experiences",
+  ExperiencesController.GetAllExperiences
+);
 // TODO GET /me/experiences => ritorna le esperienze dell'utente loggato
-Router.get("/me/experiences", ExperiencesController.GetLoggedUserExperiences);
+ExperiencesRouter.get(
+  "/me/experiences",
+  ExperiencesController.GetLoggedUserExperiences
+);
 // TODO POST /experiences => crea una nuova esperienza per l'utente loggato
-Router.post("/experiences", ExperiencesController.PostNewExperience);
+ExperiencesRouter.post("/experiences", ExperiencesController.PostNewExperience);
 // TODO PUT /:expId => caricamento immagine per esperienza
-Router.put("/:expId", ExperiencesController.PutUploadExperienceImage);
+ExperiencesRouter.put(
+  "/:expId",
+  ExperiencesController.PutUploadExperienceImage
+);
 // TODO EX : PUT /:expId => modifica le esperienze
-Router.put("/:expId", ExperiencesController.PutUpdateExperience);
+ExperiencesRouter.put("/:expId", ExperiencesController.PutUpdateExperience);
 // TODO EX : DELETE /:expId => elimina le esperienze
-Router.delete("/:expId", ExperiencesController.DeleteExperience);
-export default Router;
+ExperiencesRouter.delete("/:expId", ExperiencesController.DeleteExperience);
+export default ExperiencesRouter;
